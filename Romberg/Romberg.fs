92066a75b39d111bef99f59bec9b0db1ad80a428
AndrewGaspar <andrew.james.gaspar@gmail.com>
2013-12-08 16:31:20 -0600

    Finished!

--- Romberg/Romberg.fs
+++ Romberg/Romberg.fs
@@ -1,4 +1,36 @@
-﻿namespace Romberg
+﻿namespace NumericalAnalysis
 
-type Class1() = 
-    member this.X = "F#"
+open System
+
+module Integration =
+
+    let private power n x = 
+        [1..x+1] |> Seq.reduce (fun memo _ -> memo * n)
+
+    let romberg f lower upper steps =
+        let range = upper - lower
+
+        let h i = range / (Math.Pow(2.0,float(i)))
+
+        let RValues = Array2D.create (steps+1) (steps+1) Double.NaN
+
+        let r term =
+            match term with
+            | (0,0) -> range/2.0 * ((f upper) + (f lower))
+            | (i,0) -> 
+                let hi = h i
+                let sum =
+                    [0..((power 2 (i-1))-1)] 
+                    |> Seq.map (fun k -> f(lower + float(2*k+1)*hi))
+                    |> Seq.sum
+
+                (RValues.[i-1,0])/2.0 + hi * sum
+            | (i, j) ->
+                (RValues.[i, j-1] +
+                    1.0/(float((power 4 j) - 1))*(RValues.[i, j-1] - RValues.[i-1,j-1]))
+
+        for j = 0 to steps do
+            for i = j to steps do
+                RValues.[i,j] <- r (i,j)
+
+        r (steps,steps)
--- Romberg/Tests.fsx
+++ Romberg/Tests.fsx
@@ -1,8 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
-open Romberg
+#load "Romberg.fs"
+open NumericalAnalysis.Integration
+open System
 
 // Define your library scripting code here
 
+let f x = 8.0 * (Math.Sqrt(1.0 - x*x) - x)
+
+romberg f 0.0 (1.0/Math.Sqrt(2.0)) 9 |> printfn "%.12f"
