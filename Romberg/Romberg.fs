1b54634830400c3764ea7b77304a48a0ca9eaeac
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-12-11 13:04:35 -0600

    Updated Romber to use ** rather than own power method. Added better test.

--- Romberg/Romberg.fs
+++ Romberg/Romberg.fs
@@ -4,13 +4,10 @@ open System
 
 module Integration =
 
-    let private power n x = 
-        [1..x+1] |> Seq.reduce (fun memo _ -> memo * n)
-
     let romberg f lower upper steps =
         let range = upper - lower
 
-        let h i = range / (Math.Pow(2.0,float(i)))
+        let h i = range / (2.0**float(i))
 
         let RValues = Array2D.create (steps+1) (steps+1) Double.NaN
 
@@ -20,14 +17,14 @@ module Integration =
             | (i,0) -> 
                 let hi = h i
                 let sum =
-                    [0..((power 2 (i-1))-1)] 
+                    [0..(int(2.0**float(i-1))-1)] 
                     |> Seq.map (fun k -> f(lower + float(2*k+1)*hi))
                     |> Seq.sum
 
                 (RValues.[i-1,0])/2.0 + hi * sum
             | (i, j) ->
                 (RValues.[i, j-1] +
-                    1.0/(float((power 4 j) - 1))*(RValues.[i, j-1] - RValues.[i-1,j-1]))
+                    1.0/(4.0**float(j) - 1.0)*(RValues.[i, j-1] - RValues.[i-1,j-1]))
 
         for j = 0 to steps do
             for i = j to steps do
--- Romberg/Tests.fsx
+++ Romberg/Tests.fsx
@@ -7,6 +7,8 @@ open System
 
 // Define your library scripting code here
 
-let f x = 8.0 * (Math.Sqrt(1.0 - x*x) - x)
+//let f x = 8.0 * (Math.Sqrt(1.0 - x*x) - x)
+//romberg f 0.0 (1.0/Math.Sqrt(2.0)) 9 |> printfn "%.12f"
 
-romberg f 0.0 (1.0/Math.Sqrt(2.0)) 9 |> printfn "%.12f"
+let erf' t = 2.0 / Math.Sqrt(Math.PI) * (Math.E**(-(t**2.0)))
+let erf x = romberg erf' 0.0 x 9
